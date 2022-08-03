8c800436b3a63a7f502092cf38074bd59d839588
Jamyth <jamyth0712@gmail.com>
2022-08-03 11:16:03 +0800

    [REFACTOR]: update build script and conf

--- script/build.ts
+++ script/build.ts
@@ -1,7 +1,6 @@
 import { ViteBuilder } from "vite-runner";
 import { splitVendorChunkPlugin } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
-import vitePluginRequire from "vite-plugin-require";
 import path from "path";
 
 new ViteBuilder({
@@ -9,5 +8,5 @@ new ViteBuilder({
     outDirectory: path.join(__dirname, "../build"),
     tsconfigPath: path.join(__dirname, "../config/tsconfig.src.json"),
     useReact: true,
-    plugins: [vitePluginRequire(), VitePWA(), splitVendorChunkPlugin()],
+    plugins: [VitePWA(), splitVendorChunkPlugin()],
 }).build();
