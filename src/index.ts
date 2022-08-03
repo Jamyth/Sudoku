941150f138640c7cf230089ca548d88d2ab5c755
Jamyth <jamyth0712@gmail.com>
2022-08-03 18:52:41 +0800

    [REFACTOR]: test sw

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { async, createApp } from "react-shiba";
 import { ErrorHandler } from "util/ErrorHandler";
 import { ErrorComponent } from "util/temp";
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({
+    onRegistered: (r) => {
+        r?.update();
+    },
+});
 
 const Component = async(() => import("module/main"), "MainComponent", { ErrorComponent });
 
