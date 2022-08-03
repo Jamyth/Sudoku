6613fb5d53a8ebcab23c1ab6e6e8b7b25a522183
Jamyth <jamyth0712@gmail.com>
2022-08-03 19:58:37 +0800

    [REFACTOR]: fix non-draft mode clear draft

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import { async, createApp } from "react-shiba";
 import { ErrorHandler } from "util/ErrorHandler";
 import { ErrorComponent } from "util/temp";
-import { registerSW } from "virtual:pwa-register";
+// import { registerSW } from "virtual:pwa-register";
 
-registerSW({
-    onRegistered: (r) => {
-        r?.update();
-    },
-});
+// const updateTime = 60 * 60 * 1000
+
+// registerSW({
+//     onRegistered: (r) => {
+//         r?.update();
+//     },
+// });
 
 const Component = async(() => import("module/main"), "MainComponent", { ErrorComponent });
 
