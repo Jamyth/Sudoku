23998c08cc298a68ff45b1a282c5ff322349576d
Jamyth <jamyth0712@gmail.com>
2022-08-03 19:42:36 +0800

    [REFACTOR]: fix non-draft mode clear draft

--- src/module/game/index.ts
+++ src/module/game/index.ts
@@ -157,7 +157,7 @@ class ModuleGameModule extends Module<Path, State> {
             };
         }
 
-        if (value === correctAnswer) {
+        if (value === correctAnswer && this.selectMode === null) {
             board.forEach((row) => {
                 row.forEach((_) => {
                     const isSameGrid = SudokuUtil.isInSameGrid(cell.row, cell.column, _.row, _.column);
