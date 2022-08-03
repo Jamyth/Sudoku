5bf2fdbd9f9fef425ff638cfbf2561b863024f31
Jamyth <jamyth0712@gmail.com>
2022-08-03 17:05:22 +0800

    [FEATURE]: added pwa feature

--- script/build.ts
+++ script/build.ts
@@ -8,5 +8,57 @@ new ViteBuilder({
     outDirectory: path.join(__dirname, "../build"),
     tsconfigPath: path.join(__dirname, "../config/tsconfig.src.json"),
     useReact: true,
-    plugins: [VitePWA(), splitVendorChunkPlugin()],
+    plugins: [
+        VitePWA({
+            registerType: "autoUpdate",
+            manifest: {
+                name: "老婆專用數獨",
+                short_name: "Wife's Sudoku",
+                description: "Jamyth's version of Sudoku, no ads",
+                icons: [
+                    {
+                        src: "icon-72x72.png",
+                        sizes: "72x72",
+                        type: "image/png",
+                    },
+                    {
+                        src: "icon-96x96.png",
+                        sizes: "96x96",
+                        type: "image/png",
+                    },
+                    {
+                        src: "icon-128x128.png",
+                        sizes: "128x128",
+                        type: "image/png",
+                    },
+                    {
+                        src: "icon-144x144.png",
+                        sizes: "144x144",
+                        type: "image/png",
+                    },
+                    {
+                        src: "icon-152x152.png",
+                        sizes: "152x152",
+                        type: "image/png",
+                    },
+                    {
+                        src: "icon-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "icon-384x384.png",
+                        sizes: "384x384",
+                        type: "image/png",
+                    },
+                    {
+                        src: "icon-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+            },
+        }),
+        splitVendorChunkPlugin(),
+    ],
 }).build();
